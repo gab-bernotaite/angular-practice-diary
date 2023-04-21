93e5811d920837f9478f7658380475672ba92644
Gabriele Bernotaite <gabriele.bernotaite@gabriele.bernotaite's Mac>
2023-04-21 13:21:03 +0300

    home page was created

---
+++ src/app/home-page/home-page.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+import '../app.component.css'
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css'],
+})
+
+export class HomePageComponent {
+
+  
+
+
+}
